dbde790592784053324d557e58d88412a0965e91
Vinicius Vasconi <villasboas.micska@gmail.com>
2024-12-03 01:10:29 -0300

    Adicionadas as soluções (c++) para os problemas Oracle of alexandria e trinomial triangle.

---
+++ BeeCrowd/trinomialTriangle.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+ 
+int main() {
+    
+    int row =0;
+    long int sum = 0;
+    std::cin >> row;
+    sum = std::pow(3,row);
+    std::cout << sum  << std::endl;
+    
+    return 0;
+}
